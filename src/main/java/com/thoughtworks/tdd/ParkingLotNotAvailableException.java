d821ee0712cfd679b3ed51bcadd46a8db26f250e
retr0 <395637649@qq.com>
2019-07-15 19:38:45 +0800

    [Hendricks] refactor:duplicated code in ParkingLotNotAvailableException

--- src/main/java/com/thoughtworks/tdd/ParkingLotNotAvailableException.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLotNotAvailableException.java
@@ -1,10 +1,6 @@
 package com.thoughtworks.tdd;
 
 public class ParkingLotNotAvailableException extends Exception {
-    public ParkingLotNotAvailableException() {
-
-    }
-
     public ParkingLotNotAvailableException(String message) {
         super(message);
     }
