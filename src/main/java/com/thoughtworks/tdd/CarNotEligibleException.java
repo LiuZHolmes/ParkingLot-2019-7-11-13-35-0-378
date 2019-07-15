1fa24da006b9ea5663f9e2cef97ed0c89d073cc8
retr0 <395637649@qq.com>
2019-07-15 19:38:14 +0800

    [Hendricks] refactor:duplicated code in CarNotEligibleException

--- src/main/java/com/thoughtworks/tdd/CarNotEligibleException.java
+++ src/main/java/com/thoughtworks/tdd/CarNotEligibleException.java
@@ -1,10 +1,6 @@
 package com.thoughtworks.tdd;
 
 public class CarNotEligibleException extends Exception {
-    public CarNotEligibleException(String message) {
-        super(message);
-    }
-
     public CarNotEligibleException() {
     }
 }
