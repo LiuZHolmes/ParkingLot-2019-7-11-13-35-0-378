835e51d9d06eba0af1d0ff97b5d2d14b69e837ca
retr0 <395637649@qq.com>
2019-07-15 19:23:38 +0800

    [Hendricks] refactor:duplicated code in SmartParkingBoy/parkCarThenReturnTicket

--- src/main/java/com/thoughtworks/tdd/SmartParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/SmartParkingBoy.java
@@ -3,17 +3,13 @@ package com.thoughtworks.tdd;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
-public class SmartParkingBoy extends ParkingBoy{
+public class SmartParkingBoy extends ParkingBoy {
     @Override
-    public Ticket parkCarThenReturnTicket(Car car) throws Exception {
-        if (isParkingLotAvailable() && isCarEligible(car)) {
-            Ticket ticket = new Ticket();
-            parkingLots.stream().sorted(Comparator.comparing(ParkingLot::getEmptyPositions).reversed())
-                    .collect(Collectors.toList()).get(0)
-                    .getLot().put(ticket, car);
-            return ticket;
-        } else {
-            throw new Exception();
-        }
+    public Ticket parkCar(Car car) {
+        Ticket ticket = new Ticket();
+        parkingLots.stream().sorted(Comparator.comparing(ParkingLot::getEmptyPositions).reversed())
+                .collect(Collectors.toList()).get(0)
+                .getLot().put(ticket, car);
+        return ticket;
     }
 }
