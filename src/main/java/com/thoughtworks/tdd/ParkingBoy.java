092b15e3e9ecb788cb510b793953a5d07e02255a
retr0 <395637649@qq.com>
2019-07-15 19:16:05 +0800

    [Hendricks] refactor:extract method in ParkingBoy/parkCarThenReturnTicket

--- src/main/java/com/thoughtworks/tdd/ParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -15,19 +15,21 @@ public class ParkingBoy {
     protected List<ParkingLot> parkingLots;
 
     public ParkingBoy() {
-        parkingLots = IntStream.rangeClosed(1,defaultNumberOfParkingLots).boxed().map(x -> new ParkingLot()).collect(Collectors.toList());
+        parkingLots = IntStream.rangeClosed(1, defaultNumberOfParkingLots).boxed().map(x -> new ParkingLot()).collect(Collectors.toList());
     }
 
     public Ticket parkCarThenReturnTicket(Car car) throws Exception {
-        if (isParkingLotAvailable() && isCarEligible(car)) {
-            Ticket ticket = new Ticket();
-            parkingLots.stream().filter(ParkingLot::isAvailable)
-                    .collect(Collectors.toList()).get(0)
-                    .getLot().put(ticket, car);
-            return ticket;
-        } else {
-            throw new Exception();
-        }
+        testIfParkingIsDoable(car);
+        Ticket ticket = new Ticket();
+        parkingLots.stream().filter(ParkingLot::isAvailable)
+                .collect(Collectors.toList()).get(0)
+                .getLot().put(ticket, car);
+        return ticket;
+    }
+
+    private void testIfParkingIsDoable(Car car) throws ParkingLotNotAvailableException, CarNotEligibleException {
+        isParkingLotAvailable() ;
+        isCarEligible(car);
     }
 
     public void fetchCarByTicket() throws TicketNotEligibleException {
@@ -53,7 +55,7 @@ public class ParkingBoy {
     protected boolean isCarEligible(Car car) throws CarNotEligibleException {
         if (parkingLots.stream().noneMatch(x -> x.getLot().containsValue(car)) && car != nullCar)
             return true;
-        else throw  new CarNotEligibleException();
+        else throw new CarNotEligibleException();
     }
 
     private boolean isTicketEligible(Ticket ticket) throws TicketNotEligibleException {
@@ -66,7 +68,7 @@ public class ParkingBoy {
         return parkingLots.get(index).isAvailable();
     }
 
-    public List<Integer> getOccupationOfParkingLots(){
+    public List<Integer> getOccupationOfParkingLots() {
         return parkingLots.stream().map(ParkingLot::getOccupation).collect(Collectors.toList());
     }
 }
